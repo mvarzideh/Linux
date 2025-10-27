dcdd0ee2c09cd933380e256dd562b08c9ebf3ed6
mvarzideh <m.varzideh@gmail.com>
2025-10-27 16:16:48 +0330

    Add updated file

--- myapp.py
+++ myapp.py
@@ -1,8 +1,8 @@
 import random
 value1=int(input("please enter an integer:\n"))
 value2=int(input("please enter an integer:\n"))
+if value1<10 or value2< 10:
+    print("please enter an integer grater than 10")
 
 x=random.randint(value1,value2)
-print(x)
-print("End of program")
-print("done")
+print("Your Random Number is:",x,sep='')
